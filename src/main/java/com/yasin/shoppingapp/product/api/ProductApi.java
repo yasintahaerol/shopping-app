60e67eef190b53dbd0bedf1ac85a5ea916c2f0aa
Yasin Taha Erol <yasintahaerol@gmail.com>
2021-01-18 14:12:25 +0300

    added getAll method and some fix

--- src/main/java/com/yasin/shoppingapp/product/api/ProductApi.java
+++ src/main/java/com/yasin/shoppingapp/product/api/ProductApi.java
@@ -1,10 +1,22 @@
 package com.yasin.shoppingapp.product.api;
 
+import com.yasin.shoppingapp.product.domain.Product;
+import com.yasin.shoppingapp.product.model.ProductResponse;
+import com.yasin.shoppingapp.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductApi {
 
+    private final ProductService productService;
+    @GetMapping
+    public Flux<ProductResponse> getAll() {
+        return productService.getAll();
+    }
 }
